38b11c65caca2631e7403427e9c4a65e10d4b016
system64fumo <system64fumo@tuta.io>
2024-05-30 16:46:54 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+EXEC = syslock
+PKGS = gtkmm-4.0 gtk4-layer-shell-0 pam
+SRCS +=	$(wildcard src/*.cpp)
+OBJS = $(SRCS:.cpp=.o)
+DESTDIR = $(HOME)/.local
+
+CXXFLAGS = -march=native -mtune=native -Os -s -Wall
+CXXFLAGS += $(shell pkg-config --cflags $(PKGS))
+LDFLAGS += $(shell pkg-config --libs $(PKGS))
+
+$(EXEC): $(OBJS)
+	$(CXX) -o $(EXEC) $(OBJS) \
+	$(LDFLAGS) \
+	$(CXXFLAGS)
+
+%.o: %.cpp
+	$(CXX) $(CFLAGS) -c $< -o $@ \
+	$(CXXFLAGS)
+
+install: $(EXEC)
+	mkdir -p $(DESTDIR)/bin
+	install $(EXEC) $(DESTDIR)/bin/$(EXEC)
+
+clean:
+	rm $(EXEC) $(SRCS:.cpp=.o)
---
+++ src/pam.cpp
@@ -0,0 +1,33 @@
+#include "pam.hpp"
+
+int pam_conversation(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
+	struct pam_response *response = (struct pam_response *)malloc(num_msg * sizeof(struct pam_response));
+	if (response == NULL) {
+		return PAM_CONV_ERR;
+	}
+
+	for (int i = 0; i < num_msg; ++i) {
+		if (msg[i]->msg_style == PAM_PROMPT_ECHO_OFF || msg[i]->msg_style == PAM_PROMPT_ECHO_ON) {
+			const char *password = (const char *)appdata_ptr;
+			response[i].resp = strdup(password);
+			response[i].resp_retcode = 0;
+		} else {
+			free(response);
+			return PAM_CONV_ERR;
+		}
+	}
+
+	*resp = response;
+	return PAM_SUCCESS;
+}
+
+bool authenticate(char *user, const char *password) {
+	struct pam_conv conv = { pam_conversation, (void *)password };
+	pam_handle_t *pamh = NULL;
+
+	int retval = pam_start("login", user, &conv, &pamh);
+	retval = pam_authenticate(pamh, 0);
+
+	pam_end(pamh, retval);
+	return (retval == PAM_SUCCESS);
+}
