d65252f3a550ea1cbc8dbe708f2e0807b652bb1f
system64fumo <system64fumo@tuta.io>
2024-05-30 22:18:03 +0300

    Separate window into it's own file
    Move window related things to window.cpp and window.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -1,17 +1,8 @@
-#include <gtkmm.h>
+#pragma once
 
-Glib::RefPtr<Gtk::Application> app;
+#include "window.hpp"
 
-class syslock : public Gtk::Window {
-
-	public:
-		syslock();
-
-	private:
-		Gtk::Box box_layout;
-		Gtk::Image image_profile;
-		Gtk::Label label_username;
-		Gtk::Entry entry_password;
-
-		void on_entry();
-};
+Glib::RefPtr<Gdk::Pixbuf> create_circular_pixbuf(const Glib::RefPtr<Gdk::Pixbuf>& src_pixbuf);
+inline Glib::RefPtr<Gtk::Application> app;
+inline int profile_scale = 128;
+inline syslock* win;
---
+++ src/window.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <gtkmm.h>
+
+class syslock : public Gtk::Window {
+
+	public:
+		syslock();
+
+	private:
+		Gtk::Box box_layout;
+		Gtk::Image image_profile;
+		Gtk::Label label_username;
+		Gtk::Entry entry_password;
+
+		void on_entry();
+};
+
