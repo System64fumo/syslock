0bd3837be7024c5551d078d50a67a75668b530cd
system64fumo <system64fumo@protonmail.com>
2024-06-02 21:19:21 +0300

    Cleanup
    Move ext session lock stuff to auth.cpp and auth.hpp
    Ideally i'd like to have an auth class containing everything auth related
    That can come at a later date

--- src/auth.hpp
+++ src/auth.hpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <security/pam_appl.h>
-#include <cstring>
+#include <gtkmm.h>
+#include <wayland-client.h>
+
+inline struct wl_display *display = nullptr;
+inline struct wl_registry *registry = nullptr;
+inline struct wl_output *output = nullptr;
+inline struct ext_session_lock_manager_v1 *session_lock_manager = nullptr;
+inline struct ext_session_lock_v1 *session_lock = nullptr;
+inline struct ext_session_lock_surface_v1 *lock_surface = nullptr;
+inline struct wl_surface *wl_surface = nullptr;
 
 int pam_conversation(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr);
 bool authenticate(char *user, const char *password);
+void lock_session(Gtk::Window &window);
+void unlock_session();
